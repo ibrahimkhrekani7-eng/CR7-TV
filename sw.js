42f064a4a17a19e640c1312601a72fe711e0df3f
ibrahimkhrekani7-eng <ibrahimkhrekani7@gmail.com>
2026-03-11 08:20:15 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,27 @@
 const CACHE_NAME = 'iptv-master-v5';
-const ASSETS_TO_CACHE = ['./', './index.html', './admin.html', './src/style.css', './src/app.js', './src/admin.js', './src/firebase-config.js', './manifest.json'];
-self.addEventListener('install', (e) => e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS_TO_CACHE))));
-self.addEventListener('fetch', (e) => e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request))));
+const ASSETS_TO_CACHE = [
+    './',
+    './index.html',
+    './admin.html',
+    './src/style.css',
+    './src/app.js',
+    './src/admin.js',
+    './src/firebase-config.js',
+    './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
